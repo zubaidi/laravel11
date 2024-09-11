8c4a5bd33010acac90c66bd94c13bd56254a2e1d
zubaidi <muhammadzubaidi333@gmail.com>
2024-09-11 11:54:58 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutCOntroller;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('dashboard'));
-Route::get('/about', fn () => view('about'));
+Route::get('/', DashboardController::class);
+Route::get('/about', [AboutCOntroller::class, 'index']);
 Route::get('/gallery', fn () => view('gallery'));
 Route::get('/login', fn () => view('login'));
